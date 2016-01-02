baf0a942863e18bed92fd21108cf7a87c42329b1
rochaeinar <einar_21@hotmail.com>
2016-01-01 22:15:08 -0400

    Add "Fall to fill field" message on error logs

--- DataAccessLayer/dal/src/main/java/com/erc/dal/DB.java
+++ DataAccessLayer/dal/src/main/java/com/erc/dal/DB.java
@@ -152,52 +152,59 @@ public class DB {
     }
 
     private static void fillFields(ArrayList<java.lang.reflect.Field> fields, Cursor cursor, Object entity) throws IllegalAccessException {
-        for (java.lang.reflect.Field field : fields) {
-            Object value = null;
-            Type type = field.getType();
-            do {
-                if (type.equals(String.class)) {
-                    value = cursor.getString(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
+        Type type;
+        String currentField = "null";
+        try {
+            for (java.lang.reflect.Field field : fields) {
+                Object value = null;
+                type = field.getType();
+                currentField = field.getName();
+                do {
+                    if (type.equals(String.class)) {
+                        value = cursor.getString(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
+                        break;
+                    }
+                    if (type.equals(char.class) || type.equals(Character.class)) {
+                        String charText = cursor.getString(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
+                        value = Util.isNullOrEmpty(charText) ? '\0' : charText.toCharArray()[0];
+                        break;
+                    }
+                    if (type.equals(Date.class)) {
+                        String dateIso = cursor.getString(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
+                        value = HelperDate.getDateFromFormat(dateIso, HelperDate.ISO_FORMAT);
+                        break;
+                    }
+                    if (type.equals(short.class) || type.equals(Short.class)) {
+                        value = cursor.getShort(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
+                        break;
+                    }
+                    if (type.equals(int.class) || type.equals(Integer.class)) {
+                        value = cursor.getInt(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
+                        break;
+                    }
+                    if (type.equals(long.class) || type.equals(Long.class)) {
+                        value = cursor.getLong(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
+                        break;
+                    }
+                    if (type.equals(boolean.class) || type.equals(Boolean.class)) {
+                        value = cursor.getInt(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field))) == 1;
+                        break;
+                    }
+                    if (type.equals(double.class) || type.equals(Double.class)) {
+                        value = cursor.getDouble(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
+                        break;
+                    }
+                    if (type.equals(float.class) || type.equals(Float.class)) {
+                        value = cursor.getFloat(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
+                        break;
+                    }
                     break;
-                }
-                if (type.equals(char.class) || type.equals(Character.class)) {
-                    String charText = cursor.getString(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
-                    value = Util.isNullOrEmpty(charText) ? '\0' : charText.toCharArray()[0];
-                    break;
-                }
-                if (type.equals(Date.class)) {
-                    String dateIso = cursor.getString(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
-                    value = HelperDate.getDateFromFormat(dateIso, HelperDate.ISO_FORMAT);
-                    break;
-                }
-                if (type.equals(short.class) || type.equals(Short.class)) {
-                    value = cursor.getShort(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
-                    break;
-                }
-                if (type.equals(int.class) || type.equals(Integer.class)) {
-                    value = cursor.getInt(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
-                    break;
-                }
-                if (type.equals(long.class) || type.equals(Long.class)) {
-                    value = cursor.getLong(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
-                    break;
-                }
-                if (type.equals(boolean.class) || type.equals(Boolean.class)) {
-                    value = cursor.getInt(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field))) == 1;
-                    break;
-                }
-                if (type.equals(double.class) || type.equals(Double.class)) {
-                    value = cursor.getDouble(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
-                    break;
-                }
-                if (type.equals(float.class) || type.equals(Float.class)) {
-                    value = cursor.getFloat(cursor.getColumnIndex(ReflectionHelper.getDataBaseNameOfField(field)));
-                    break;
-                }
-                break;
-            } while (true);
+                } while (true);
 
-            field.set(entity, value);
+                field.set(entity, value);
+            }
+        } catch (Exception e) {
+            Log.e("Failed to fill Field: \"" + currentField + "\" in " + entity.getClass().getName(), e);
         }
     }
 }
